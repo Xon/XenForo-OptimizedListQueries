52fc41a165183531c69540789886335f4323694f
Xon <tazipper1@hotmail.com>
2015-08-13 20:46:14 +0800

    Better detection for hoisting left joins out of the inner loop, ensure the function returns if an exception when executing the optimized query.

--- upload/library/SV/OptimizedListQueries/XenForo/Model/Conversation.php
+++ upload/library/SV/OptimizedListQueries/XenForo/Model/Conversation.php
@@ -21,7 +21,8 @@ class SV_OptimizedListQueries_XenForo_Model_Conversation extends XFCP_SV_Optimiz
         }
         else
         {
-            $escaped = preg_replace("/[\s\)]left\s+join[\s\(]/i", " ", ' '.strtolower($innerJoin));
+            $escaped = preg_replace('/[\r\n]+/', " ", ' '.strtolower($innerJoin));
+            $escaped = preg_replace('/[\s\)]left\s+join[\s\(]/', ' ', $escaped);
             if (strpos($escaped, 'join') !== false)
             {
                 $safe = false;
@@ -32,29 +33,38 @@ class SV_OptimizedListQueries_XenForo_Model_Conversation extends XFCP_SV_Optimiz
             $innerJoin = '';
         }
 
-        return $this->fetchAllKeyed('
-                SELECT message.*,
-                    user.*, IF(user.username IS NULL, message.username, user.username) AS username,
-                    user_profile.*,
-                    user_privacy.*
-                    ' . $joinOptions['selectFields'] . '
-                FROM ( ' . $this->limitQueryResults('
-                SELECT message.message_id
-                FROM xf_conversation_message AS message
-                    ' . $innerJoin . '
-                WHERE message.conversation_id = ?
-                ORDER BY message.message_date
-                ', $limitOptions['limit'], $limitOptions['offset']
-                ). ') ConvMessageId
-                JOIN xf_conversation_message AS message on message.message_id = ConvMessageId.message_id
-                LEFT JOIN xf_user AS user ON
-                    (user.user_id = message.user_id)
-                LEFT JOIN xf_user_profile AS user_profile ON
-                    (user_profile.user_id = message.user_id)
-                LEFT JOIN xf_user_privacy AS user_privacy ON
-                    (user_privacy.user_id = message.user_id)
-                ' . $joinOptions['joinTables'] . ' 
-                ORDER BY message.message_date '
-            , 'message_id', $conversationId);
+        try
+        {
+            return $this->fetchAllKeyed('
+                    SELECT message.*,
+                        user.*, IF(user.username IS NULL, message.username, user.username) AS username,
+                        user_profile.*,
+                        user_privacy.*
+                        ' . $joinOptions['selectFields'] . '
+                    FROM ( ' . $this->limitQueryResults('
+                    SELECT message.message_id
+                    FROM xf_conversation_message AS message
+                        ' . $innerJoin . '
+                    WHERE message.conversation_id = ?
+                    ORDER BY message.message_date
+                    ', $limitOptions['limit'], $limitOptions['offset']
+                    ). ') ConvMessageId
+                    JOIN xf_conversation_message AS message on message.message_id = ConvMessageId.message_id
+                    LEFT JOIN xf_user AS user ON
+                        (user.user_id = message.user_id)
+                    LEFT JOIN xf_user_profile AS user_profile ON
+                        (user_profile.user_id = message.user_id)
+                    LEFT JOIN xf_user_privacy AS user_privacy ON
+                        (user_privacy.user_id = message.user_id)
+                    ' . $joinOptions['joinTables'] . '
+                    ORDER BY message.message_date '
+                , 'message_id', $conversationId);
+        }
+        catch(Exception $e)
+        {
+            // we choice poorly an generated an error
+            XenForo_Error::logException($e, false, 'error running optimized query');
+            return parent::getThreadsInForum($forumId, $conditions, $fetchOptions);
+        }
     }
 }
--- upload/library/SV/OptimizedListQueries/XenForo/Model/Thread.php
+++ upload/library/SV/OptimizedListQueries/XenForo/Model/Thread.php
@@ -16,22 +16,68 @@ class SV_OptimizedListQueries_XenForo_Model_Thread extends XFCP_SV_OptimizedList
         $sqlClauses = $this->prepareThreadFetchOptions($fetchOptions);
         $forceIndex = (!empty($fetchOptions['forceThreadIndex']) ? 'FORCE INDEX (' . $fetchOptions['forceThreadIndex'] . ')' : '');
 
-        return $this->fetchAllKeyed('
-                SELECT thread.*
-                    ' . $sqlClauses['selectFields'] . '
-                FROM (
-                '. $this->limitQueryResults('
-                    SELECT thread.thread_id
-                    FROM xf_thread AS thread ' . $forceIndex . '
-                    ' . $sqlClauses['joinTables'] . '
-                    WHERE ' . $whereConditions . '
-                    ' . $sqlClauses['orderClause'] . '
-                ', $limitOptions['limit'], $limitOptions['offset']
-                ) . ') threadId
-                JOIN xf_thread AS thread on thread.thread_id = threadId.thread_id '
-            . $sqlClauses['joinTables']. '
-            ' . $sqlClauses['orderClause']
-            , 'thread_id');
+        $safe = true;
+        $innerJoin =  $sqlClauses['joinTables'];
+        // look for constructs we know affect the inner join
+        if (preg_match('/^\s*,/', $innerJoin))
+        {
+            $safe = false;
+        }
+        else
+        {
+            $whitespace_normalized = preg_replace('/[\r\n]+/', ' ', ' '.strtolower($innerJoin));
+            $escaped = preg_replace('/[\s\)]left\s+join[\s\(]/', ' ', $whitespace_normalized);
+            if (strpos($escaped, 'join') !== false)
+            {
+                $safe = false;
+            }
+            else
+            {
+                $matches = array();
+                if (preg_match_all('/[\s\)]left\s+join\s+[`\w]+\s+(as\s+){0,1}([`\w]+)/', $whitespace_normalized, $matches))
+                {
+                    foreach($matches[2] as $match)
+                    {
+                        if (strpos($whereConditions, $match) !== false)
+                        {
+                            $safe = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($safe)
+        {
+            $innerJoin = '';
+        }
+
+        try
+        {
+            return $this->fetchAllKeyed('
+                    SELECT thread.*
+                        ' . $sqlClauses['selectFields'] . '
+                    FROM (
+                    '. $this->limitQueryResults('
+                        SELECT thread.thread_id
+                        FROM xf_thread AS thread ' . $forceIndex . '
+                        ' . $innerJoin . '
+                        WHERE ' . $whereConditions . '
+                        ' . $sqlClauses['orderClause'] . '
+                    ', $limitOptions['limit'], $limitOptions['offset']
+                    ) . ') threadId
+                    JOIN xf_thread AS thread on thread.thread_id = threadId.thread_id '
+                . $sqlClauses['joinTables']. '
+                ' . $sqlClauses['orderClause']
+                , 'thread_id');
+        }
+        catch(Exception $e)
+        {
+            // we choice poorly an generated an error
+            XenForo_Error::logException($e, false, 'error running optimized query');
+            return parent::getThreadsInForum($forumId, $conditions, $fetchOptions);
+        }
     }
 
 }
