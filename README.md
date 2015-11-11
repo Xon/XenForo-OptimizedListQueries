bae3918e1f97155f20f03a25524e89bde25d2575
Xon <tazipper1@hotmail.com>
2015-11-12 00:06:46 +0800

    Implement node-list caching

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 Optimized query for getting threads in a forum with large number of threads, or for conversations with a very high page count.
 
-The conversations & threads use limit & offset to implement paging through the result set. 
+The conversations & forums use limit & offset to implement paging through the result set. 
+
+Uses a better performance session activity query.
 
 MySQL implements 'early row lookup' which results in the large select statement pulling in more data than is required.
 - http://stackoverflow.com/questions/4481388/why-does-mysql-higher-limit-offset-slow-the-query-down
 - http://explainextended.com/2009/10/23/mysql-order-by-limit-performance-late-row-lookups/
 
 Ideally conversations would implement the position system like posts do, which would be even faster than using sub-selects to force 'late row lookup'.
+
+
+## Optional caching 
+- Forum node lists.
---
+++ upload/library/SV/OptimizedListQueries/XenForo/Model/Node.php
@@ -0,0 +1,95 @@
+<?php
+
+class SV_OptimizedListQueries_XenForo_Model_Node extends XFCP_SV_OptimizedListQueries_XenForo_Model_Node
+{
+    public function getUniqueNodeTypeIdsFromNodeGrouped(array $nodes)
+    {
+        $output = array();
+        foreach ($nodes AS $group)
+        {
+            foreach ($group AS $node)
+            {
+                $output[$node['node_type_id']] = true;
+            }
+        }
+
+        return array_keys($output);
+    }
+
+    public function getNodeDataForListDisplay($parentNode, $displayDepth, array $nodePermissions = null)
+    {
+        // fast path only if custom permisions aren't being used or a cache isn't defined
+        $cacheObject = $this->_getCache(true);
+        if ($nodePermissions !== null || empty($cacheObject))
+        {
+            return parent::getNodeDataForListDisplay($parentNode, $displayDepth, $nodePermissions);
+        }
+
+        $options = XenForo_Application::getOptions();
+        $visitor = XenForo_Visitor::getInstance();
+        $this->getModelFromCache('XenForo_Model_User');
+        if ($visitor['permission_combination_id'] == XenForo_Model_User::$guestPermissionCombinationId)
+        {
+            $expiry = $options->sv_cache_nodes_guests;
+        }
+        else
+        {
+            $expiry = $options->sv_cache_nodes_members;
+        }
+        // not caching for this type of user
+        if (!$expiry)
+        {
+            return parent::getNodeDataForListDisplay($parentNode, $displayDepth, $nodePermissions);
+        }
+
+
+        if (is_array($parentNode))
+        {
+            $parentNodeId = $parentNode['node_id'];
+        }
+        else if ($parentNode === false)
+        {
+            $parentNodeId = 0;
+        }
+        else
+        {
+            throw new XenForo_Exception('Unexpected parent node parameter passed to getNodeDataForListDisplay');
+        }
+
+
+
+        $cacheId = 'nodelist_'.$parentNodeId.'_'.$displayDepth.'_'.$visitor['permission_combination_id'];
+        if ($raw = $cacheObject->load($cacheId, true))
+        {
+            $nodeList = @unserialize($raw);
+            if ($nodeList !== false)
+            {
+                $nodeList['nodeHandlers'] = $this->getNodeHandlersForNodeTypes($this->getUniqueNodeTypeIdsFromNodeGrouped($nodeList['nodesGrouped']));
+                return $nodeList;
+            }
+        }
+
+        if ($parentNodeId)
+        {
+            $nodes = $this->getChildNodes($parentNode, true);
+        }
+        else
+        {
+            $nodes = $this->getAllNodes(false, true);
+        }
+
+        $nodeList = $this->getNodeListDisplayData($nodes, $parentNodeId, $displayDepth, $nodePermissions);
+
+        if ($cacheObject)
+        {
+            // nodeHandlers are objects, so remove them from being cached.
+            $nodeHandlers = $nodeList['nodeHandlers'];
+            $nodeList['nodeHandlers'] = null;
+            $raw = serialize($nodeList);
+            $nodeList['nodeHandlers'] = $nodeHandlers;
+            $cacheObject->save($raw, $cacheId, array(), $expiry);
+        }
+
+        return $nodeList;
+    }
+}
