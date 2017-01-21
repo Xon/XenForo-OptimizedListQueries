8803a8f9cc9c55a22be428ed459fbe1c86e88c21
Xon <tazipper1@hotmail.com>
2017-01-21 16:52:30 +0800

    Add caching of the session list

--- upload/library/SV/OptimizedListQueries/XenForo/ControllerPublic/Forum.php
+++ upload/library/SV/OptimizedListQueries/XenForo/ControllerPublic/Forum.php
@@ -4,19 +4,59 @@ class SV_OptimizedListQueries_XenForo_ControllerPublic_Forum extends XFCP_SV_Opt
 {
     protected function _getSessionActivityList()
     {
+        $cacheObject = XenForo_Application::getCache();
+        $options = XenForo_Application::getOptions();
+        $visitor = XenForo_Visitor::getInstance();
+        $this->getModelFromCache('XenForo_Model_User');
+
+        $expiry = false;
+        if (!empty($cacheObject))
+        {
+            if ($visitor['permission_combination_id'] == XenForo_Model_User::$guestPermissionCombinationId)
+            {
+                $expiry = $options->sv_cache_membersonline_query_guests;
+            }
+            else
+            {
+                // todo ...
+            }
+        }
+
+        if ($expiry)
+        {
+            $cacheId = 'sessionlist_0';
+            if ($raw = $cacheObject->load($cacheId, true))
+            {
+                $data = @json_decode($raw, true);
+                if ($data)
+                {
+                    return $data;
+                }
+            }
+        }
+
         if (!XenForo_Application::getOptions()->sv_membersonline_query)
         {
-            return parent::_getSessionActivityList();
+            $data = parent::_getSessionActivityList();
         }
-        $visitor = XenForo_Visitor::getInstance();
+        else
+        {
+            /** @var $sessionModel XenForo_Model_Session */
+            $sessionModel = $this->getModelFromCache('XenForo_Model_Session');
 
-        /** @var $sessionModel XenForo_Model_Session */
-        $sessionModel = $this->getModelFromCache('XenForo_Model_Session');
+            $data = $sessionModel->getSessionActivityQuickListFast(
+                $visitor->toArray(),
+                array('cutOff' => $sessionModel->getOnlineStatusTimeout()),
+                ($visitor['user_id'] ? $visitor->toArray() : null)
+            );
+        }
+        
+        if ($expiry)
+        {
+            $raw = json_encode($data);
+            $cacheObject->save($raw, $cacheId, array(), $expiry);
+        }
 
-        return $sessionModel->getSessionActivityQuickListFast(
-            $visitor->toArray(),
-            array('cutOff' => $sessionModel->getOnlineStatusTimeout()),
-            ($visitor['user_id'] ? $visitor->toArray() : null)
-        );
+        return $data;
     }
 }
